b50ae8a4310772a58701d292e14809cf50adaadc
ParkSeungHwan01 <psh092401@kookmin.ac.kr>
2024-01-07 21:27:04 +0900

    menubar layout

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+const Nav = () => {
+  return (
+    <nav>
+      <ul>
+        <li>서비스 이름</li>
+        <li>마이</li>
+        <li>스터디 메이트</li>
+        <li>자료</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
